eb52d9b01811d71d55512297a29dbdba8922d32e
tienpahm <phamtienacu@gmail.com>
2021-10-20 03:21:28 +1100

    test edge case and fix bug

--- DriveIQ-PHP-API.php
+++ DriveIQ-PHP-API.php
@@ -1,11 +1,48 @@
 <?php
+//Try to catch error
+if($argc != 3 ) {
+    exit("Please enter category and limit") ; 
+} ;
+
+
+if(intval($argv[2]) == 0) {
+    exit("limit must be a number greater than 0");
+}
+//Create category list 
+$categoryList = array("Animals" , "Anime" , "Blockchain", "Books", "Business","Calendar","Weather","Transportation","Health","Jobs","Music");
+
+function isCategory() {
+    global $categoryList;
+    global $argv;
+    foreach($categoryList as $item){
+        if(strcmp(strtolower($item) ,strtolower($argv[1]) ) == 0) {
+            return true ;
+        }
+    }
+    return false ; 
+}
+
+if(!isCategory()){
+    exit("No result");
+}
 
 
 
-$url = "https://api.publicapis.org/entries" ; 
 
-//Call API to get data
-$data=json_decode(file_get_contents($url),false);
+//If input accepted
+print_r("Application is running...");
+
+try{
+
+    //Call API to get data
+    $url = "https://api.publicapis.org/entries" ; 
+    $data=json_decode(file_get_contents($url),false);
+
+}catch(Exception $e){
+    print_r($e->getMessage());
+    exit();
+}
+
 
 
 
@@ -15,38 +52,32 @@ $data=json_decode(file_get_contents($url),false);
     Algo : Loop by filter and return valid value
     Output: Print the result with match Category an amount = limit  
 */
-
+function getFilterData(){
     $counter = 0 ; 
-    $filteredData = [] ; 
-
+    $filteredData = [] ;
+    global $data ; 
+    global $argv ;
     for ($i=0; $i < count($data->entries); $i++) { 
-        if($counter <= $argv[2]) {
-            if(strcmp($data->entries[$i]->Category,$argv[1]) == 0)
+        if($counter <= $argv[2] ) {
+            if(strcmp(strtolower($data->entries[$i]->Category),strtolower($argv[1])) == 0)
             {   
                 array_push($filteredData , $data->entries[$i]);    
-                print_r($counter);
+                $counter++ ;
             }
         }   
     }
-    usort($filteredData , function($a, $b) {return strcmp($a->API, $b->API);});
-    print_r($filteredData);
- 
-    // $filteredItems = array_filter($data->entries, function($item) use($counter , $limit , $category ) {
-    //     print_r($counter);
-    //     if($counter <= $limit) {
-    //         print_r($counter);
-    //         if(strcmp($item->Category,$category) == 0)
-    //         {       
-    //             $counter++ ; 
-    //             return $item ;
-    //         }
-    //     }   
-    // });
-    // print_r(count($filteredItems));
+    return $filteredData;
+}
 
+    $filteredData = getFilterData() ; 
 
-
-// print_r($argv) ;
+    if(count($filteredData) == 0) {
+        print_r("No result");
+    } else {
+        usort($filteredData , function($a, $b) {return strcmp(strtolower($a->API), strtolower($b->API));});
+        print_r($filteredData);
+    }
+    
     
 
 ?>
